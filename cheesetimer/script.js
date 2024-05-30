7fb5f6fa10a0f47f7a1a4aafe3300d4564c17683
Fat-Robot-Blaze <94099246+Fat-Robot-Blaze@users.noreply.github.com>
2024-05-30 01:54:22 -0400

    made the time 100% accurate

--- cheesetimer/script.js
+++ cheesetimer/script.js
@@ -5,75 +5,59 @@ let resetBtn = document.getElementById('reset');
 let addBtn = document.getElementById('add');
 let add2Btn = document.getElementById('add2');
 
-let hour = 00;
-let minute = 00;
-let second = 00;
-let count = 00;
+let hour = 0;
+let minute = 0;
+let second = 0;
+let count = 0;
+let startDate = Date.now()
+
 
 startBtn.addEventListener('click', function () {
     timer = true;
+    startDate = Date.now()
     stopWatch();
 });
-addBtn.addEventListener('click', function () {
-    hour++;
+addBtn.addEventListener('click', function () { //hour
+    startDate -= reverseCalcHours(1);
 });
-add2Btn.addEventListener('click', function () {
-    minute++;
-});
-stopBtn.addEventListener('click', function () {
-    timer = false;
+add2Btn.addEventListener('click', function () { //minute
+    startDate -= reverseCalcHours(1 / 60);
 });
 
 resetBtn.addEventListener('click', function () {
-    timer = false;
-    hour = 0;
-    minute = 0;
-    second = 0;
-    count = 0;
-    document.getElementById('hr').innerHTML = "00";
-    document.getElementById('min').innerHTML = "00";
-    document.getElementById('sec').innerHTML = "00";
-    document.getElementById('count').innerHTML = "00";
+    startDate = Date.now()
 });
 
 function stopWatch() {
     if (timer) {
-        count++;
 
-        if (count == 100) {
-            second++;
-            count = 0;
-        }
+        var mils = Date.now() - startDate;
+        var hrs = calcHours(mils);
 
-        if (second == 60) {
-            minute++;
-            second = 0;
-        }
+        let timeLeft = hrs;
 
-        if (minute == 60) {
-            hour++;
-            minute = 0;
-            second = 0;
-        }
+        let hrString = Math.floor(timeLeft);
+        timeLeft -= hrString;
+        let minString = Math.floor(timeLeft * 60);
+        timeLeft -= (minString / 60);
+        let secString = Math.floor(timeLeft * 60 * 60);
+        timeLeft -= ((secString / 60) / 60);
+        let countString = Math.round(timeLeft * 60 * 60 * 100);
 
-        let hrString = hour;
-        let minString = minute;
-        let secString = second;
-        let countString = count;
 
-        if (hour < 10) {
+        if (hrString < 10) {
             hrString = "0" + hrString;
         }
 
-        if (minute < 10) {
+        if (minString < 10) {
             minString = "0" + minString;
         }
 
-        if (second < 10) {
+        if (secString < 10) {
             secString = "0" + secString;
         }
 
-        if (count < 10) {
+        if (countString < 10) {
             countString = "0" + countString;
         }
 
@@ -82,10 +66,30 @@ function stopWatch() {
         document.getElementById('sec').innerHTML = secString;
         document.getElementById('count').innerHTML = countString;
 
-        document.getElementById("timebar").value = Math.min(((Number(hour) + (Number(minute) / 60)) / 24) * 10000, 10000)
-        
+        document.getElementById("timebar").value = Math.min(((Date.now() - startDate) / reverseCalcHours(24)) * 10000, 10000)
         setTimeout(stopWatch, 10);
 
 
     }
+}
+
+
+
+function reverseCalcHours(milliseconds) {
+
+
+    const seconds = 1000 * Number(milliseconds);
+    const minute = seconds * 60;
+    const hour = minute * 60;
+
+    return hour;
+}
+
+function calcHours(hour) {
+
+    const minute = Number(hour) / 60
+    const seconds = minute / 60;
+    const milliseconds = seconds / 1000;
+
+    return milliseconds;
 }
